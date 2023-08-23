14f9199135da811d69c85b3f88d591c1233ef4f2
Xiao <z.xiao01@ufromail.cl>
2023-08-23 10:03:30 -0400

    subo cosas version 0.0

--- src/calculandoEnBinario.java
+++ src/calculandoEnBinario.java
@@ -1,2 +1,18 @@
-public class calculandoEnBinario {
+import java.util.Scanner;
+class calculandoEnBinario {
+    public static void main(String[] args) {
+        byte numero =ingresarNumero();
+        System.out.println("el numero ingresado es :"+ numero);
+    }
+    public static byte ingresarNumero() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("ingrese un numero entero no menor qeu 0 y no mayor que 255:");
+        byte numero = input.nextByte();
+        if (numero < 0){
+            System.out.println("error, numero negativo");
+        }
+
+        return numero;
+    }
+
 }
